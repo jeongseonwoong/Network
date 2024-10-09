4b1c24445cd2cb7ffc478d6696b42e1e587db101
jeongseonwoong <tjsdnd1219@naver.com>
2024-10-09 12:17:06 +0900

    FileLayer thread 구현

--- ipc2023/FileLayer.cpp
+++ ipc2023/FileLayer.cpp
@@ -5,6 +5,8 @@
 #include "stdafx.h"
 #include "pch.h"
 #include "FileLayer.h"
+#include<vector>
+#include<algorithm>
 
 #ifdef _DEBUG
 #undef THIS_FILE
@@ -38,16 +40,30 @@ CFileLayer::~CFileLayer()
 	END_CATCH
 }
 
-BOOL CFileLayer::Send(unsigned char* ppayload, int nlength)
+
+
+BOOL CFileLayer::Send(unsigned char* ppayload, int nlength)//EthernetFrame으로 전송할 때
 {
+	string file_name = "IpcBuff.txt";
+
 	TRY
 	{
-		CFile m_FileDes(_T("IpcBuff.txt"),
-						 CFile::modeCreate | CFile::modeWrite);
+		CFile m_FileDes(file_name.c_str(), CFile::modeCreate | CFile::modeWrite);
 	//////////////////////// fill the blank ///////////////////////////////
 			// 파일 생성
-			m_FileDes.Write(ppayload,nlength);
-			m_FileDes.Close();
+	if (nlength <= 1488) 
+	{
+		m_FileDes.Write(ppayload, nlength);
+		m_FileDes.Close();		
+	}
+	else {
+		ThreadParams* pParams = new ThreadParams;
+		pParams->ppayload = ppayload;
+		pParams->nlength = nlength;
+		pParams->pFileLayer = this;
+		AfxBeginThread(FileThread, pParams);
+	}
+
 			///////////////////////////////////////////////////////////////////////
 	}
 		CATCH(CFileException, e)
@@ -62,11 +78,12 @@ BOOL CFileLayer::Send(unsigned char* ppayload, int nlength)
 		return TRUE;
 }
 
-BOOL CFileLayer::Receive()
+BOOL CFileLayer::Receive() //EthernetLayer로부터 받을때
 {
+	string file_name = "IpcBuff.txt";
 	TRY
 	{
-		CFile m_FileDes(_T("IpcBuff.txt"), CFile::modeRead);
+		CFile m_FileDes(file_name.c_str(), CFile::modeRead);
 
 	//////////////////////// fill the blank ///////////////////////////////
 			// 파일의 내용을 가져온다.
@@ -101,3 +118,59 @@ BOOL CFileLayer::Receive()
 
 		return TRUE;
 }
+
+UINT CFileLayer::FileThread(LPVOID pParam)
+{
+	using namespace std;
+	// pParam을 ThreadParams 타입으로 캐스팅
+	ThreadParams* pParams = (ThreadParams*)pParam;
+
+	// 인수 추출
+	unsigned char* ppayload = pParams->ppayload;
+	int nlength = pParams->nlength;
+
+	const int CHUNK_SIZE = 1488;
+	int chunkCount = (nlength + CHUNK_SIZE - 1) / CHUNK_SIZE; // 총 조각 개수 계산
+	std::vector<FILE_FRAGMENT> payloadChunks;
+
+	// 데이터를 1488바이트씩 나누어 FILE_FRAGMENT 구조체에 저장
+	for (int i = 0; i < chunkCount; ++i) {
+		FILE_FRAGMENT fragment = {};
+		fragment.fapp_totlen = nlength;
+		fragment.fapp_seq_num = i;
+
+		// 조각의 타입 설정
+		if (chunkCount == 1) {
+			fragment.fapp_type = 0x00; // 단편화되지 않음
+		}
+		else if (i == 0) {
+			fragment.fapp_type = 0x01; // 단편화의 첫 부분
+		}
+		else if (i == chunkCount - 1) {
+			fragment.fapp_type = 0x03; // 단편화의 마지막 부분
+		}
+		else {
+			fragment.fapp_type = 0x02; // 단편화의 중간 부분
+		}
+
+		// 남은 바이트와 CHUNK_SIZE 중 작은 값을 선택하여 마지막 조각을 처리
+		int bytesToWrite = min(CHUNK_SIZE, nlength - i * CHUNK_SIZE);
+
+		// 데이터를 fapp_data에 복사
+		memcpy(fragment.fapp_data, ppayload + i * CHUNK_SIZE, bytesToWrite);
+
+		// 메시지 종류 설정 (필요에 따라 변경 가능)
+		fragment.fapp_msg_type = 0x01; // 예: 일반 데이터
+
+		// 벡터에 조각 추가
+		payloadChunks.push_back(fragment);
+	}
+
+	// 이후 payloadChunks 벡터를 사용하여 조각별로 데이터 전송 또는 처리 가능
+
+	// 동적으로 할당한 메모리 해제
+	delete pParams;	
+
+	return 0; // 스레드 종료
+}
+
--- ipc2023/FileLayer.h
+++ ipc2023/FileLayer.h
@@ -1,9 +1,10 @@
-// FileLayer.h: interface for the CFileLayer class.
+﻿// FileLayer.h: interface for the CFileLayer class.
 //
 //////////////////////////////////////////////////////////////////////
 
 #if !defined(AFX_FILELAYER_H__D67222B3_1B00_4C77_84A4_CEF6D572E181__INCLUDED_)
 #define AFX_FILELAYER_H__D67222B3_1B00_4C77_84A4_CEF6D572E181__INCLUDED_
+#define MAX_APP_DATA 1500
 
 #if _MSC_VER > 1000
 #pragma once
@@ -21,6 +22,28 @@ public:
 
 	CFileLayer(char* pName);
 	virtual ~CFileLayer();
+
+	static UINT FileThread(LPVOID pParam);
+	typedef struct FILE_Fragmet
+	{
+		unsigned long fapp_totlen; // 총 길이
+		unsigned short fapp_type; // 데이터 타입 단편화에 대한 정보를 담을 수 있음 ex) 0x00 – 단편화 되지 않음, 0x01 – 단편화 첫부분, 0x02 – 단편화 중간, 등등.
+
+		unsigned char fapp_msg_type; // 메시지 종류
+		unsigned char unused; // 사용 안함
+		unsigned long fapp_seq_num; // fragmentation 순서 ❖ seq_num에서 이 단편화된 조각이 몇 번째 조각인지 저장 
+
+		unsigned char fapp_data[MAX_APP_DATA];
+	} FILE_FRAGMENT, * PFILE_FRAGMENT;
+
+
+	typedef struct _ThreadParams {
+		unsigned char* ppayload;
+		int nlength;
+		CFileLayer* pFileLayer;
+	} ThreadParams;
+
+
 };
 
 #endif // !defined(AFX_FILELAYER_H__D67222B3_1B00_4C77_84A4_CEF6D572E181__INCLUDED_)
