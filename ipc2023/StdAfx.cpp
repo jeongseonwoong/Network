02cc2f4102440a425e7c4042a3f43d52e4eb76de
GAUDUL <137772044+GAUDUL@users.noreply.github.com>
2024-10-09 14:39:51 +0900

    Delete ipc2023 directory

--- ipc2023/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	ipc.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-#include "pch.h"
-
-
