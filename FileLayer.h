78f6afda671fec9f0681fe47e620bfb324114897
GAUDUL <137772044+GAUDUL@users.noreply.github.com>
2024-10-10 21:02:50 +0900

    FileLayer 헤더 설정

--- FileLayer.h
+++ FileLayer.h
@@ -15,12 +15,21 @@
 class CFileLayer
 	: public CBaseLayer
 {
+private:
+	inline void ResetFileHeader();
+
 public:
+	unsigned char* m_ppayload;
+	int m_length;
+
 	BOOL	Receive();
 	BOOL	Send(unsigned char* ppayload, int nlength);
 
+	static UINT FileThread(LPVOID pParm); //파일 전송 thread
+
 	CFileLayer(char* pName);
 	virtual ~CFileLayer();
+
 	typedef struct _FILE_APP {
 		unsigned long fapp_totlen; //파일의 총 길이
 		unsigned short fapp_type; //단편화 데이터 타입
@@ -28,6 +37,14 @@ public:
 		unsigned char unused; // 사용 X
 		unsigned long fapp_seq_num; //단편화된 조각 순서
 		unsigned char fapp_data[MAX_APP_DATA]; //파일 데이터
+	}FILE_APP_HEADER, *FILE_APP_HEADER_POINTER;
+
+protected:
+	FILE_APP_HEADER m_fileHeader;
+	enum {
+		FAPP_TYPE_BEGIN = 0x00,	// 전송된 나눠진 패킷 처음 부분
+		FAPP_TYPE_CONT = 0x01,	// 전송된 나눠진 패킷 중간 부분
+		FAPP_TYPE_END = 0x02  // 전송된 나눠진 패킷 끝 부분
 	};
 };
 
