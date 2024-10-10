0e13df52f5d316f2a5057ad442b5dab536e525d0
GAUDUL <137772044+GAUDUL@users.noreply.github.com>
2024-10-10 18:49:16 +0900

    Update FileLayer.h

--- FileLayer.h
+++ FileLayer.h
@@ -21,6 +21,14 @@ public:
 
 	CFileLayer(char* pName);
 	virtual ~CFileLayer();
+	typedef struct _FILE_APP {
+		unsigned long fapp_totlen; //파일의 총 길이
+		unsigned short fapp_type; //단편화 데이터 타입
+		unsigned char fapp_msg_type; //메시지(확장자?) 종류
+		unsigned char unused; // 사용 X
+		unsigned long fapp_seq_num; //단편화된 조각 순서
+		unsigned char fapp_data[MAX_APP_DATA] //파일 데이터
+	};
 };
 
 #endif // !defined(AFX_FILELAYER_H__D67222B3_1B00_4C77_84A4_CEF6D572E181__INCLUDED_)
